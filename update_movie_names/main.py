16399cf3b524df98b48e93ef735a7bfb06eedbc6
Tianrui Li <littealeaf.2001@gmail.com>
2021-05-25 11:51:28 -0700

    Updates scripts

--- update_movie_names/main.py
+++ update_movie_names/main.py
@@ -1,43 +1,25 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-
-
-def add_movie_titles_on_page(driver, titles):
-    title_elements = driver.find_elements_by_css_selector('h3.lister-item-header')
-    for title_element in title_elements:
-        title = title_element.find_element_by_css_selector('a').text
-        titles.append(title)
-
+import requests
+from bs4 import BeautifulSoup
 
 def update_movie_names(event, context):
-    options = Options()
-    # Get rid of headless options to see what script is doing in a browser
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),
-                              options=options)
-
-    imdb_websites = [
-        'https://www.imdb.com/search/title/?groups=top_1000',
-        'https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc',
-        'https://www.imdb.com/search/title/?groups=top_1000&sort=num_votes,desc',
-        'https://www.imdb.com/search/title/?groups=top_1000&sort=boxoffice_gross_us,desc',
-        'https://www.imdb.com/search/title/?groups=top_1000&sort=release_date,desc'
+    imdb_base_websites = [
+        'https://www.imdb.com/search/title/?groups=top_1000&start=',
+        # 'https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&start=',
+        # 'https://www.imdb.com/search/title/?groups=top_1000&sort=num_votes,desc&start=',
+        # 'https://www.imdb.com/search/title/?groups=top_1000&sort=boxoffice_gross_us,desc&start=',
+        # 'https://www.imdb.com/search/title/?groups=top_1000&sort=release_date,desc&start='
     ]
 
-    movie_titles = []
-    for website in imdb_websites:
-        driver.get(website)
-        add_movie_titles_on_page(driver, movie_titles)
+    for website in imdb_base_websites:
+        for i in range(1, 1000, 50):
+            imdb_website = website + str(i)
+            r = requests.get(imdb_website)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            title_elements = soup.find_all('h3', 'lister-item-header')
+            for title_element in title_elements:
+                title = title_element.a.string
+                print(title)
 
-        NUM_NEXTS = 19
-        for x in range(NUM_NEXTS):
-            next_button = driver.find_element_by_css_selector('a.lister-page-next.next-page')
-            next_button.click()
-            add_movie_titles_on_page(driver, movie_titles)
-            driver.implicitly_wait(500)
 
-    for movie_title in movie_titles:
-        print(movie_title + '\n')
+# update_movie_names(None, None)
 
-    driver.close()
---
+++ update_movie_names_selenium/main.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
+def add_movie_titles_on_page(driver, titles):
+    title_elements = driver.find_elements_by_css_selector('h3.lister-item-header')
+    for title_element in title_elements:
+        title = title_element.find_element_by_css_selector('a').text
+        titles.append(title)
+
+
+def update_movie_names_selenium():
+    options = Options()
+    # Get rid of headless options to see what script is doing in a browser
+    # options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),
+                              options=options)
+
+    imdb_websites = [
+        'https://www.imdb.com/search/title/?groups=top_1000',
+        'https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc',
+        'https://www.imdb.com/search/title/?groups=top_1000&sort=num_votes,desc',
+        'https://www.imdb.com/search/title/?groups=top_1000&sort=boxoffice_gross_us,desc',
+        'https://www.imdb.com/search/title/?groups=top_1000&sort=release_date,desc'
+    ]
+
+    movie_titles = []
+    # for website in imdb_websites:
+    driver.get(imdb_websites[0])
+    add_movie_titles_on_page(driver, movie_titles)
+
+    NUM_NEXTS = 2
+    for x in range(NUM_NEXTS):
+        next_button = driver.find_element_by_css_selector('a.lister-page-next.next-page')
+        next_button.click()
+        add_movie_titles_on_page(driver, movie_titles)
+        driver.implicitly_wait(500)
+
+    for movie_title in movie_titles:
+        print(movie_title)
+
+    print('All your favorite movies with just one click ;P')
+    driver.close()
+
+
+update_movie_names_selenium()
